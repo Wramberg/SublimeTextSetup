a61f7789c36ebcbef8e6af8f2ee447bcd104d0c7
Wramberg <jesper.wramberg@gmail.com>
2017-04-08 21:37:07 +0200

    Simplify custom module for tab rotation

--- Data/Packages/RotateBetweenTabsInGroup/RotateBetweenTabsInGroup.py
+++ Data/Packages/RotateBetweenTabsInGroup/RotateBetweenTabsInGroup.py
@@ -6,12 +6,7 @@ class RotateToNextTabInGroup(sublime_plugin.WindowCommand):
       active_view = self.window.active_view()
       active_grp, index = self.window.get_view_index(active_view)
       views = self.window.views_in_group(active_grp)
-
-      if index == (len(views) - 1):
-         next_index = 0
-      else:
-         next_index = index + 1
-
+      next_index = (index + 1) % len(views)
       self.window.focus_view(views[next_index])
 
 
@@ -20,10 +15,5 @@ class RotateToPrevTabInGroup(sublime_plugin.WindowCommand):
       active_view = self.window.active_view()
       active_grp, index = self.window.get_view_index(active_view)
       views = self.window.views_in_group(active_grp)
-
-      if index == 0:
-         next_index = len(views) - 1
-      else:
-         next_index = index - 1
-
+      next_index = (index - 1) % len(views)
       self.window.focus_view(views[next_index])
